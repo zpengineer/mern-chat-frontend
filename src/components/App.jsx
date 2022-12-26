f2c4e180a8fdb1f06749ae25edd7d5d71191496b
Oleh Fedorov <clickprime94@gmail.com>
2022-12-26 13:46:57 +0200

    Add main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { AppBar } from './AppBar/AppBar';
+import { Main } from './Main/Main';
 
 export const App = () => {
   return (
     <>
       <AppBar />
+      <Main />
     </>
   );
 };
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+export const Main = () => {
+  return (
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">1 of 3</div>
+        <div class="col-6">2 of 3 (wider)</div>
+        <div class="col">3 of 3</div>
+      </div>
+      <div class="row">
+        <div class="col">1 of 3</div>
+        <div class="col-5">2 of 3 (wider)</div>
+        <div class="col">3 of 3</div>
+      </div>
+    </div>
+  );
+};
